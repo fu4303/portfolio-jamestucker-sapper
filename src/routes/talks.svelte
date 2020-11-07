733b2f4e4d54ae4561f54226378a51a32e9aad06
James C. Tucker <jamesctucker@protonmail.com>
2020-11-07 16:35:05 -0500

    updated date format for talks/podcasts

--- src/routes/talks.svelte
+++ src/routes/talks.svelte
@@ -16,25 +16,25 @@
   <tbody>
     <tr>
       <td>The Art of Tech</td>
-      <td>September 15, 2020</td>
+      <td>9/15/20</td>
       <td>Denver Startup Week</td>
       <td>Virtual</td>
     </tr>
     <tr>
       <td>The Art of Tech</td>
-      <td>September 8, 2020</td>
+      <td>9/8/20</td>
       <td>Twin Cities Startup Week</td>
       <td>Virtual</td>
     </tr>
     <tr>
       <td>Vue Crash Course</td>
-      <td>August 19, 2020</td>
+      <td>8/19/20</td>
       <td>Prime Alumni Workshop</td>
       <td>Virtual</td>
     </tr>
     <tr>
       <td>Digital Wellness</td>
-      <td>April 10, 2020</td>
+      <td>4/10/20</td>
       <td>Prime Guest Speaker Series</td>
       <td>Prime Digital Academy</td>
     </tr>
@@ -54,12 +54,12 @@
   <tbody>
     <tr>
       <td><a href="https://develomentor.com/2020/11/02/james-tucker-theology-major-becomes-software-developer-105/">Theology Major Becomes Software Developer</a></td>
-      <td>November 2, 2020</td>
+      <td>11/2/20</td>
       <td>Develomentor</td>
     </tr>
     <tr>
       <td><a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy8xM2IxMjNhNC9wb2RjYXN0L3Jzcw/episode/NDRkYjBhMTktOWEwZi00MDFhLTlmZmYtM2IwOTI2YmJjN2Zk?sa=X&ved=0CAUQkfYCahcKEwjglPn46efsAhUAAAAAHQAAAAAQAg&hl=en">Starting Out in Development</a></td>
-      <td>September 8, 2020</td>
+      <td>9/8/20</td>
       <td>The Frontend Podcast</td>
     </tr>
   </tbody>
