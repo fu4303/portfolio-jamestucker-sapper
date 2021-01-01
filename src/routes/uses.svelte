5184f9241fdfc917c8ca0c93a50f3a7eb7a5e394
James C. Tucker <jamesctucker@protonmail.com>
2021-01-01 17:37:09 -0600

    added a uses page

--- src/routes/uses.svelte
+++ src/routes/uses.svelte
@@ -2,5 +2,103 @@
 	<title>Uses</title>
 </svelte:head>
 
-<h1>Uses</h1>
-<p>Coming soon!</p>
+<style>
+	.link {
+		font-size: 2rem;
+	}
+</style>
+
+<h1 class="page-header">Uses</h1>
+<p>Apparently sharing one's use stack is a thing in the tech world, so here's a collection of software, hardware, and audio/visual equipment I use the most.</p>
+
+<h2>Hardware</h2>
+<ul>
+	<li>
+		<a class="link" href="https://support.apple.com/kb/SP776?locale=en_US">
+			2018 Macbook Pro 15 (16GB RAM, 256GB SSD)
+		</a>work computer
+	</li>
+	<li>
+		<a class="link" href="https://www.apple.com/in/mac/compare/?modelList=Macbook-Air-M1,MacBookPro-13-M1,Macbook-Air-2017">
+			2020 Macbook Air (M1 chip)
+		</a>personal computer
+	</li>
+	<li>
+		<a class="link" href="https://www.lg.com/us/monitors/lg-34wl750-b-ultrawide-monitor">
+			LG 34in Ultrawide Monitor
+		</a>second display
+	</li>
+	<li><a class="link" href="https://www.apple.com/shop/product/MLA22LL/A/magic-keyboard-us-english">Magic Keyboard</a> daily keyboard</li>
+	<li><a class="link" href="https://www.apple.com/shop/product/MJ2R2LL/A/magic-trackpad-2-silver">Magic Trackpad 2</a> daily "mouse"</li>
+</ul>
+
+<h2>Software</h2>
+<h3>Development</h3>
+<ul>
+	<li><a class="link" href="https://code.visualstudio.com/">VS Code</a> feature-reach, multi-language code editor</li>
+	<li><a class="link" href="https://iterm2.com/">iTerm2</a> alternative to the default MacOS terminal</li>
+	<li><a class="link" href="https://trello.com/en-US">Trello</a> "kanban" style boards for tracking todos and bugs</li>
+	<li><a class="link" href="https://www.postman.com/">Postman</a> powerful app for developing APIs; mainly used for testing REST requests</li>
+	<li><a class="link" href="https://www.google.com/chrome/">Chrome</a> powerful dev-tools</li>
+</ul>
+
+<h3>Design</h3>
+<ul>
+	<li><a class="link" href="https://www.figma.com/">Figma</a> powerful interface designer that is somehow free</li>
+	<li><a class="link" href="https://miro.com/">Miro</a> collaborative design boards</li>
+	<li><a class="link" href="https://www.adobe.com/products/photoshop-lightroom.html">Adobe Lightroom</a> where I edit most of my personal photography</li>
+</ul>
+
+<h3>Misc</h3>
+<ul>
+	<li><a class="link" href="https://culturedcode.com/">Things 3</a> beautiful todo tracker</li>
+	<li><a class="link" href="https://roamresearch.com/">Roam Research</a> my second brain & place for taking notes</li>
+	<li><a class="link" href="https://readwise.io/">Readwise</a> a library for web clippings, article and Kindle highlights; syncs them all to Roam</li>
+	<li><a class="link" href="https://www.airr.io/">Airr</a> podcast app that transcribes audio clips you save</li>
+	<li><a class="link" href="https://freedom.to/">Freedom</a> blocks distracting websites</li>
+	<li><a class="link" href="https://apps.apple.com/us/app/be-focused-pro-focus-timer/id961632517?mt=12">Be Focused Pro</a> fancy Pomodoro timer</li>
+	<li><a class="link" href="https://ia.net/writer">iA Writer</a> main writing/markdown tool</li>
+	<li><a class="link" href="https://magnet.crowdcafe.com/">Magnet</a> simple app for window management with keyboard shortcuts</li>
+	<li><a class="link" href="https://cleanshot.com/">Cleanshot X</a> feature-rich screenshot/screen capture tool; used for making GIFs as well; can annotate screenshots</li>
+</ul>
+
+<h2>Communication</h2>
+<ul>
+	<li><a class="link" href="https://protonmail.com/">Protonmail</a> personal email client focused on privacy</li>
+	<li><a class="link" href="https://slack.com/">Slack</a> work communication</li>
+	<li><a class="link" href="https://discord.com/">Discord</a> chat for personal communities</li>
+</ul>
+
+<h2>Audio</h2>
+<ul>
+	<li>
+		<a class="link" href="https://www.bose.com/en_us/products/headphones/over_ear_headphones/quietcomfort-35-wireless-ii.html">
+			Bose QC35ii
+		</a>noise-cancelling headphones used for music, live-streaming, and video calls
+	</li>
+	<li>
+		<a class="link" href="https://www.akg.com/Microphones/Condenser%20Microphones/P220.html">
+			AKG P220
+		</a>XLR mic used for live-streaming and podcasts
+	</li>
+	<li>
+		<a class="link" href="https://focusrite.com/en/usb-audio-interface/scarlett/scarlett-solo">
+			Scarlett Solo
+		</a>audio interface for the AKG P220 mic
+	</li>
+</ul>
+
+<h2>Visual</h2>
+<ul>
+	<li>
+		<a class="link" href="https://fujifilm-x.com/global/products/cameras/x100f/">
+			Fujifilm X100F
+		</a>mirrorless camera used for live-streaming and photography
+	</li>
+	<li>
+		<a class="link" href="https://www.elgato.com/en/gaming/cam-link-4k">
+			Elgato Camlink 4k
+		</a>capture card for my Fujifilm for live-streaming
+	</li>
+</ul>
+
