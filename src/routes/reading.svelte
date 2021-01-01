a5074cf091f519cbf3cdcd6b8577bcce57e4182c
James C. Tucker <jamesctucker@protonmail.com>
2021-01-01 17:37:19 -0600

    added a reading page

---
+++ src/routes/reading.svelte
@@ -0,0 +1,46 @@
+<svelte:head>
+	<title>Uses</title>
+</svelte:head>
+
+<style>
+    .year {
+        margin-left: 1rem;
+    }
+</style>
+
+<h1 class="page-header">Bookshelf</h1>
+<p>An overview of what's on my bookshelf. I've been awful at actually finishing books the last few years. Here's to hoping that changes! I might start writing brief book reviews in the near future.</p>
+
+<h2>Currently Reading</h2>
+<ul>
+    <li>
+        <a href="https://www.goodreads.com/book/show/11958983-eisenhower-in-war-and-peace">
+            Eisenhower in Peace and War
+        </a>
+    </li>
+</ul>
+
+<h2>Read</h2>
+<h3 class="year">2020</h3>
+    <ul>
+        <li>
+            <a href="https://www.goodreads.com/book/show/34962304-the-motivation-myth">
+                The Motivation Myth
+            </a>
+        </li>
+        <li>
+            <a href="
+            Range: Why Generalists Triumph in a ... - Goodreadswww.goodreads.com › book › show › 41795733-range">
+                Range: Why Generalists Triumph In A Specialized World
+            </a>
+        </li>
+        <li>
+            <a href="https://www.goodreads.com/en/book/show/43982455-the-ruthless-elimination-of-hurry">           
+                The Ruthless Elimination of Hurry
+            </a>
+        </li>
+        <li>
+            <a href="https://www.goodreads.com/book/show/3892688-the-well-grounded-rubyist">The Well-Grounded Rubyist</a>
+        </li>
+    </ul>
+<h3 class="year">2019</h3>
